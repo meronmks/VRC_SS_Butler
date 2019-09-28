ffa45ad23c217e71db2f3ac7a5d9a90869b6c770
meronmks <meronmks@users.noreply.github.com>
2019-09-28 16:00:16 +0900

    多重起動防止機構追加

--- VRC_SS_Butler/App.xaml.cs
+++ VRC_SS_Butler/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,16 +15,32 @@ namespace VRC_SS_Butler
     public partial class App : Application
     {
         private NotifyIconWrapper notifyIcon;
+        private Mutex mutex = new Mutex(false, "{7C968F36-359B-48F8-A65C-D7C415B8D426}");
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-            this.notifyIcon = new NotifyIconWrapper();
+            if(mutex.WaitOne(0, false))
+            {
+                base.OnStartup(e);
+                this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+                this.notifyIcon = new NotifyIconWrapper();
+            }
+            else
+            {
+                MessageBox.Show("すでに起動しています", "VRC SS Butler", MessageBoxButton.OK, MessageBoxImage.Error);
+                mutex.Close();
+                mutex = null;
+                this.Shutdown();
+            }
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
+            if(mutex != null)
+            {
+                mutex.ReleaseMutex();
+                mutex.Close();
+            }
             base.OnExit(e);
             this.notifyIcon.Dispose();
         }
